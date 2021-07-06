64c575b5316f021a3f57641cb7b96372c537a132
larsaars <lurzapps@gmail.com>
2021-07-06 21:33:07 +0200

    caesar unnötig kompliziert

---
+++ klausur/6.py
@@ -0,0 +1,38 @@
+def caesar(msg: str, k: int = 3) -> str:
+    out = ''
+
+    if k >= 26:
+        return ''
+
+    A, Z, a, z = ord('A'), ord('Z'), ord('a'), ord('z')
+
+    def shift_index(i: int, lower: int, upper: int) -> int:
+        i += k
+
+        if i > upper:
+            i = lower + abs(i - upper) - 1
+        elif i < lower:
+            i = upper - abs(i - lower) + 1
+
+        return i
+
+    for c in msg:
+        oc = ord(c)
+
+        if A <= oc <= Z:
+            out += chr(shift_index(oc, A, Z))
+        elif a <= oc <= z:
+            out += chr(shift_index(oc, a, z))
+        else:
+            out += c
+
+    return out
+
+
+if __name__ == '__main__':
+    test_msg = 'this is a test xyz'
+    encoded_msg = caesar(test_msg)
+
+    # print encoded and again decoded msg
+    print(encoded_msg)
+    print(caesar(encoded_msg, -3))
